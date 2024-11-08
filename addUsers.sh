878f9937def28703b13910e6ed093a63aff17c4c
Hebus79 <frederic.southon@gmail.com>
2024-11-08 12:35:28 +0100

    Update addUsers.sh

--- addUsers.sh
+++ addUsers.sh
@@ -1,1 +1,26 @@
 #!bin/bash
+
+if [ "$#" -ne 1 ] ; then
+echo "Il manque les noms d'utilisateurs en argument - Fin du script"
+exit
+
+else creation_nouvel_utilisateur
+
+fi
+
+function creation_nouvel_utilisateur() {
+        
+        if $1 "getent passwd $1 > /dev/null 2>&1"; then
+            echo "L'utilisateur $1 existe déjà."
+	        sleep 2
+            "L'utilisateur $1 existe déjà"
+            return 1
+        else
+            $1 "sudo -S useradd -m $1"
+            echo "Utilisateur $1 créé."
+	        sleep 2
+            return 0
+        fi
+    }
+
+echo "Au revoir"
